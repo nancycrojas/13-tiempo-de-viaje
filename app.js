54f05fba406736ccbcbdcf1422215d03f35c5d02
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:45:47 -0300

    Variables, datos y operadores - ejercicio 13

---
+++ app.js
@@ -0,0 +1,14 @@
+//Tiempo de viaje
+
+//Crear un programa que pida al usuario la distancia de su recorrido y mostrar en un mensaje cuánto tiempo tardaría en completar el recorrido en distintos medios de transporte, definiendo de antemano distintas velocidades para distintos medios de transporte (p. ej.: a pie: 5 km/hora, bici 10km/hora, etc.).
+
+const distancia = prompt ("Por favor ingresa la distancia de tu recorrido en kilometros");
+alert("tiempo de recorrido a pie: " + distancia / 5 + " h");
+alert("tiempo de recorrido en bicicleta: " + distancia / 10 + " h");
+alert("tiempo de recorrido en auto: " + distancia / 45 + " h");
+
+
+
+
+
+
